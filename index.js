34fc85af8c5c71213e076586e4460e88526f2f5f
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-09 16:08:34 +0000

    Prepare files for /public/app.js

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`);
 });
 
+app.use("/public", express.static(`${__dirname}/public`));
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
