2df3afc09d385cf683299cc10b766f0e122edf26
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-09 16:55:36 +0000

    Added functionality to video cutting

--- public/app.js
+++ public/app.js
@@ -12,3 +12,36 @@ ffmpeg.setLogger(({ type, message }) => {
   conversionProgess.style.color = "khaki";
   conversionProgess.textContent = `OUTPUT: ${message}`;
 });
+
+const transcode = async ({ target: { files } }) => {
+  const { name } = files[0];
+  if (ffmpeg.isLoaded() === false) {
+    await ffmpeg.load();
+  }
+  ffmpeg.FS("writeFile", name, await fetchFile(files[0]));
+  await ffmpeg.run(
+    "-i",
+    name,
+    "-ss",
+    startPoint.value,
+    "-to",
+    endPoint.value,
+    "output.mp4"
+  );
+  const data = ffmpeg.FS("readFile", "output.mp4");
+  const vidURL = URL.createObjectURL(
+    new Blob([data.buffer], {
+      type: "video/mp4",
+    })
+  );
+  console.log(vidURL);
+  preview.src = vidURL;
+  downloadButton.disabled = false;
+  downloadButton.onclick = function () {
+    let str = String(vidURL);
+    str = str.slice(42); // get's the blob name
+    saveAs(vidURL, `${str}.mp4`);
+  };
+  conversionProgess.textContent = "Conversion is done!";
+};
+file.addEventListener("change", transcode);
