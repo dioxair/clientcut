7c8e2ae784b93eef4b4674e63ae8f5fc8145052a
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-09 16:35:29 +0000

    Initialized start and end point variables

--- public/app.js
+++ public/app.js
@@ -3,6 +3,8 @@ const file = document.getElementById("fileUpload");
 const preview = document.getElementById("preview");
 const conversionProgess = document.getElementById("conversionProgess");
 const downloadButton = document.getElementById("downloadButton");
+const startPoint = document.getElementById("startPoint");
+const endPoint = document.getElementById("endPoint");
 const ffmpeg = createFFmpeg({ log: false });
 
 ffmpeg.setLogger(({ type, message }) => {
